39ad7bc57c3ad0a442d34c7bd3f63273d07a20b4
Dylan <dylancai345@gmail.com>
2022-02-28 00:47:42 -0600

    Application Upload

---
+++ src/main/java/com/laioffer/onlineOrder/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.laioffer.onlineOrder;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationConfig {
+
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.laioffer.onlineOrder.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://mysql-instance.c3s3riiwsguy.us-east-2.rds.amazonaws.com:3306/onlineOrder?autoReconnect=true&serverTimezone=UTC&createDatabaseIfNotExist=true");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("abcde28847");
+
+        return dataSource;
+    }
+
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        return hibernateProperties;
+    }
+}
+
---
+++ src/main/java/com/laioffer/onlineOrder/controller/MenuInfoController.java
@@ -0,0 +1,29 @@
+package com.laioffer.onlineOrder.controller;
+
+import com.laioffer.onlineOrder.entity.MenuItem;
+import com.laioffer.onlineOrder.entity.Restaurant;
+import com.laioffer.onlineOrder.service.MenuInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class MenuInfoController {
+
+    @Autowired
+    private MenuInfoService menuInfoService;
+
+    @RequestMapping(value = "/restaurants", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Restaurant> getRestaurants() {
+        return menuInfoService.getRestaurants();
+    }
+
+    @RequestMapping(value = "/restaurant/{restaurantId}/menu", method = RequestMethod.GET)
+    @ResponseBody
+    public List<MenuItem> getMenus(@PathVariable(value = "restaurantId") int restaurantId) {
+        return menuInfoService.getAllMenuItem(restaurantId);
+    }
+}
